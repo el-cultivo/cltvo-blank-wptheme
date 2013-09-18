47ed785df3f1a371b7d668ee1febf42f394e10dc
rbto <robertopalomino87@gmail.com>
2013-09-18 14:52:01 -0500

    JS en admin

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ add_action( 'init', 'cltvo_custom_tax' );
 add_action( 'add_meta_boxes', 'cltvo_metaboxes' );
 add_action( 'save_post', 'cltvo_save_post' );
 add_action( 'wp_enqueue_scripts', 'cltvo_js' );
-
+//add_action( 'admin_enqueue_scripts', 'cltvo_admin_js' ); //descomentar para tener JS en admin (no olvidar crear el file [admin-functions.js])
 
 
 /*	SCRIPTS
@@ -18,19 +18,26 @@ add_action( 'wp_enqueue_scripts', 'cltvo_js' );
 */
 
 function cltvo_js(){
-	wp_register_script('cltvo_functions_js', JSPATH.'functions.js', array('jquery'), false, true );
+	wp_register_script( 'cltvo_functions_js', JSPATH.'functions.js', array('jquery'), false, true );
+	wp_localize_script( 'cltvo_functions_js', 'cltvo_js_vars', cltvo_js_vars() );
+	
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('cltvo_functions_js');
+}
 
+function cltvo_admin_js(){
+	wp_register_script( 'cltvo_admin_functions_js', JSPATH.'admin-functions.js', array('jquery'), false, false );
+	wp_localize_script( 'cltvo_admin_functions_js', 'cltvo_js_vars', cltvo_js_vars() );
+
+	wp_enqueue_script('cltvo_admin_functions_js');
+}
+
+function cltvo_js_vars(){
 	$php2js_vars = array(
 		'site_url'     => home_url('/'),
 		'template_url' => get_bloginfo('template_url')
 	);
-	wp_localize_script( 'cltvo_functions_js', 'php2js_vars', $php2js_vars );
-	
-	
-	if( !is_admin() ){
-		wp_enqueue_script('jquery');
-		wp_enqueue_script('cltvo_functions_js');
-	}	
+	return $php2js_vars;
 }
 
 
