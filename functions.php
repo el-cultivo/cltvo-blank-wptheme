0330eff1190c596badedd9766b43c6e5d7f733c1
rbto <robertopalomino87@gmail.com>
2013-10-15 10:58:20 -0500

    Added: Columnas en PT, Added: Elimina tamaños default, Added: Filtros en main query, Added: Modificación de nombre de «Entradas» en admin menu

--- functions.php
+++ functions.php
@@ -4,13 +4,43 @@ define( 'JSPATH', get_template_directory_uri() . '/js/' );
 define( 'BLOGURL', get_home_url('/') );
 define( 'THEMEURL', get_bloginfo('template_url').'/' );
 
-add_theme_support( 'post-thumbnails' );
-add_action( 'init', 'cltvo_posttypes' );
-add_action( 'init', 'cltvo_custom_tax' );
-add_action( 'add_meta_boxes', 'cltvo_metaboxes' );
-add_action( 'save_post', 'cltvo_save_post' );
-add_action( 'wp_enqueue_scripts', 'cltvo_js' );
-//add_action( 'admin_enqueue_scripts', 'cltvo_admin_js' ); //descomentar para tener JS en admin (no olvidar crear el file [admin-functions.js])
+// add_theme_support( 'post-thumbnails' );
+// add_filter('intermediate_image_sizes_advanced', 'cltvo_quita_tamanos_default');
+
+// add_action( 'wp_enqueue_scripts', 'cltvo_js' );
+// add_action( 'admin_enqueue_scripts', 'cltvo_admin_js' ); //descomentar para tener JS en admin (no olvidar crear el file [admin-functions.js])
+
+// add_action( 'pre_get_posts', 'cltvo_query_mod' );
+
+// add_action( 'init', 'cltvo_posttypes' );
+// add_action( 'admin_menu', 'cltvo_nombre_entradas_menu' );
+// add_filter( 'manage_edit-post_columns', 'cltvo_nueva_col_post' );
+// add_filter( 'manage_edit-crdmn_proyecto_pt_columns', 'cltvo_nueva_col_proy' );
+// add_action( 'manage_posts_custom_column' , 'cltvo_tax_col', 10, 2 );
+// add_action( 'manage_crdmn_proyecto_pt_posts_custom_column' , 'cltvo_tax_col', 10, 2 );
+
+
+// add_action( 'init', 'cltvo_custom_tax' );
+// add_action( 'add_meta_boxes', 'cltvo_metaboxes' );
+// add_action( 'save_post', 'cltvo_save_post' );
+
+
+/*	TAMAÑOS de IMGS
+	---------------
+*/
+
+if ( function_exists( 'add_image_size' ) ) { 
+	//add_image_size( '-nombre-thumb-', 310, 230, true );
+}
+
+function cltvo_quita_tamanos_default( $sizes) {
+		
+	unset( $sizes['thumbnail']);
+	unset( $sizes['medium']);
+	unset( $sizes['large']);
+	
+	return $sizes;
+}
 
 
 /*	SCRIPTS
@@ -40,6 +70,27 @@ function cltvo_js_vars(){
 	return $php2js_vars;
 }
 
+/*	FILTROS
+	-------
+*/
+function cltvo_query_mod( $query ) {
+	if( $query->is_main_query() && !is_admin() ){
+		if ( $query->is_archive() ){
+			//Restringir query sólo a un Post Type
+			$query->set( 'post_type', 'post' );
+		}
+
+		if ( $query->is_page() ){
+			//modificar el query por completo
+			$args = array(
+				'post_type' => 'post',
+				'posts_per_page' => 9,
+				'post_status' => 'publish'
+			);
+			$query->query_vars = $args;
+		}
+	}
+}
 
 
 /*	TIPOS DE POSTS
@@ -48,14 +99,72 @@ function cltvo_js_vars(){
 
 function cltvo_posttypes(){
 	//Nombre del posttype!
-	// $args = array(
-	// 	'label' => 'Artistas',  								//nombre
-	// 	'public' => true,										//Público
-	// 	'rewrite' => array( 'slug' => 'artistas' ),				//Nombre en la url
-	// 	'has_archive' => true,									//Se puede usar en un archivo
-	// 	'supports' => array( 'title', 'editor', 'thumbnail' )	//Inupts UI en la página de new post
-	// );
-	// register_post_type( 'inter_artistas_pt', $args );		//Se registra
+	$args = array(
+		'label' => 'Artistas',  								//nombre
+		'public' => true,										//Público
+		'rewrite' => array( 'slug' => 'artistas' ),				//Nombre en la url
+		'has_archive' => true,									//Se puede usar en un archivo
+		'supports' => array( 'title', 'editor', 'thumbnail' )	//Inupts UI en la página de new post
+	);
+	register_post_type( 'inter_artistas_pt', $args );		//Se registra
+}
+
+/*	Nombre "Endradas" en Menú
+	-------------------------
+*/
+
+function cltvo_nombre_entradas_menu() {
+    global $menu;
+    global $submenu;
+    $menu[5][0] = '-Nuevo nombre-';
+    $submenu['edit.php'][5][0] = '-Nuevo nombre-';
+    $submenu['edit.php'][10][0] = '-Nuevo nombre para "Nueva Entrada"-';
+    echo '';
+}
+
+/*	Columnas en PT
+	--------------
+*/
+
+function cltvo_nueva_col_post($columns) {
+	//este call back se tiene que repetir por cada
+	//Post Type que se quiera afectar
+	$unsets = array('author', 'categories', 'tags', 'comments', 'date');
+	foreach ($unsets as $unset) unset($columns[$unset]);
+	$columns['crdmn_proyecto_tax'] = 'Proyecto';
+	$columns['tags'] = 'Etiquetas';
+	$columns['date'] = 'Fecha';
+
+	return $columns;
+}
+
+function cltvo_nueva_col_proy($columns) {
+	//cambiar el sufijo dependiendo de el nombre del Post Type
+	$unsets = array('tags', 'date');
+	foreach ($unsets as $unset) unset($columns[$unset]);
+	$columns['crdmn_cliente_tax'] = 'Cliente';
+	$columns['crdmn_servicio_tax'] = 'Servicio';
+	$columns['crdmn_proyecto_tax'] = 'Proyecto';
+	$columns['tags'] = 'Etiquetas';
+	$columns['date'] = 'Fecha';
+
+	return $columns;
+}
+
+
+function cltvo_tax_col( $column_name, $post_id ) {
+	//Muestra en la columna el nombre
+	//de las taxonomías de ese post con su link
+	$taxonomy = $column_name;
+	$post_type = get_post_type($post_id);
+	$terms = get_the_terms($post_id, $taxonomy);
+
+	if (!empty($terms) ) {
+		foreach ( $terms as $term )
+			//$post_terms[] = $term->name;
+			$post_terms[] ="<a href='edit.php?post_type={$post_type}&{$taxonomy}={$term->slug}'> " .esc_html(sanitize_term_field('name', $term->name, $term->term_id, $taxonomy, 'edit')) . "</a>";
+		echo join('', $post_terms );
+	}
 }
 
 
@@ -66,20 +175,20 @@ function cltvo_posttypes(){
 	
 function cltvo_custom_tax(){
 	//Nombre de la taxonomía
-	// $argumentos = array(						
-	// 	'labels' => array(
-	// 		'name'			=> 'Secciones',			//Nombre
-	// 		'add_new_item'	=> 'Nueva Sección',		//Nombre del botón para agregar nuevo término
-	// 		'parent_item'	=> 'Sección madre'		//Asignar el término a un término padre
-	// 	),
-	// 	'hierarchical' => true
-	// );
+	$argumentos = array(						
+		'labels' => array(
+			'name'			=> 'Secciones',			//Nombre
+			'add_new_item'	=> 'Nueva Sección',		//Nombre del botón para agregar nuevo término
+			'parent_item'	=> 'Sección madre'		//Asignar el término a un término padre
+		),
+		'hierarchical' => true
+	);
 	
-	// register_taxonomy(
-	// 	'inter_seccion_tax',						//nombre de la tax
-	// 	'inter_activi_pt',							//a qué posttype pertenece
-	// 	$argumentos
-	// );	
+	register_taxonomy(
+		'inter_seccion_tax',						//nombre de la tax
+		'inter_activi_pt',							//a qué posttype pertenece
+		$argumentos
+	);	
 }
 
 
@@ -89,33 +198,26 @@ function cltvo_custom_tax(){
 */
 	
 function cltvo_metaboxes(){
-	// add_meta_box(
-	// 	'inter_descripcion_mb',		//id
-	// 	'Descripción',				//título
-	// 	'inter_descripcion_mb',		//callback function
-	// 	'inter_activi_pt',			//post type
-	// 	'side'						//posición
-	// );
-	// add_meta_box(
-	// 	'inter_colaborador_mb',
-	// 	'Colaborador',
-	// 	'inter_colaborador_mb',
-	// 	'inter_colabora_pt',
-	// 	'side'
-	// );
+	add_meta_box(
+		'inter_descripcion_mb',		//id
+		'Descripción',				//título
+		'inter_descripcion_mb',		//callback function
+		'inter_activi_pt',			//post type
+		'side'						//posición
+	);
 }
 
-// function inter_descripcion_mb($object){
-// 	echo '<p><input type="checkbox" name="inter_descripcion_in" ';
-// 	if( get_post_meta($object->ID, 'inter_descripcion_meta') )echo "checked";
-// 	echo '> Descripción de sección</p>';
-// }
-// function inter_colaborador_mb($object){
-// 	echo '<p><label>Nombre del colaborador:</label></p>';
-// 	echo '<input name="inter_colaborador_in" type="text" value="';
-// 	echo get_post_meta($object->ID, 'inter_colaborador_meta', true);
-// 	echo '" />';
-// }
+function inter_descripcion_mb($object){
+	echo '<p><input type="checkbox" name="inter_descripcion_in" ';
+	if( get_post_meta($object->ID, 'inter_descripcion_meta') )echo "checked";
+	echo '> Descripción de sección</p>';
+}
+function inter_colaborador_mb($object){
+	echo '<p><label>Nombre del colaborador:</label></p>';
+	echo '<input name="inter_colaborador_in" type="text" value="';
+	echo get_post_meta($object->ID, 'inter_colaborador_meta', true);
+	echo '" />';
+}
 
 function crdmn_equipo_mb($object){?>
 	<div class="cltvo_multi_mb">
