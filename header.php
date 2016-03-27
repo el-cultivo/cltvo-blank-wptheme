a5aa0e521582fce0015cb31d3d4e479716682cc1
Roberto <robertopalomino87@gmail.com>
2016-03-27 11:55:17 -0600

    Edited: mensaje de IE8 ahora incluye inline styling.

--- header.php
+++ header.php
@@ -48,7 +48,8 @@
 
 	?>
 
-	<!--[if gt IE 8]><div class="cltvo-browser-check"><p>Consider <a href="http://www.google.com/intl/es/chrome/browser/" target="_blank">updating your browser</a> in order to render this site correctly.</p></div><!-->
+	<!--[if gt IE 8]><div style="z-index: 1000; padding: 5px 0; text-align: center; position: absolute; top: 0; left: 0; width: 100%; background-color: #312822;"><p style="font-family: Arial, Helvetica, sans-serif; font-size: 16px; color: white;">Consider <a style="color: #EA7640;
+	text-decoration: underline;" href="http://www.google.com/intl/es/chrome/browser/" target="_blank">updating your browser</a> in order to render this site correctly.</p></div><!-->
 <!--<![endif]-->
 
 	<!-- Aquí podría abrir el main-wrap -->
