881c4be6139c0f23023235440fd6dde394ea425b
antoniofi <antonio.figueroa@unam.mx>
2020-10-28 12:56:57 -0600

    Se agrega la configuración del webpack para que utilice browsersync en vistas, js y css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,14 @@
 const mix = require('laravel-mix');
 
 mix.sourceMaps(true, 'source-map')
-.js( 'es6/micorriza.js', 'js/functions.js')
-.sass( 'sass/mazorca.scss', './style.css')
-.options({
-    processCssUrls: false,
-});
+	.js( 'es6/micorriza.js', 'js/functions.js')
+	.sass( 'sass/mazorca.scss', './style.css')
+	.options({
+    	processCssUrls: false,
+	})
+	.browserSync({
+		open: 'local',
+		host: 'localhost',
+		proxy: 'localhost',
+		files: ['views/*.php', '*.php', 'js/*.js', '*.css']
+	});
