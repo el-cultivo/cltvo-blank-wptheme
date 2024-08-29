7e810093da0057e10025e8fd73f477c3b036f07d
Len Serrano <lenserrano@MacBook-Pro-de-Len.local>
2024-08-29 17:44:00 -0600

    Mas permisos para user editor

--- functions.php
+++ functions.php
@@ -11,3 +11,14 @@ function custom_theme_setup() {
 
 // Enganchar la función a la acción 'after_setup_theme'
 add_action('after_setup_theme', 'custom_theme_setup');
+
+function cltvo_role_edit() {
+	// get the the role object
+	$role_object = get_role( 'editor' );
+	
+    if ( !$role_object->has_cap('edit_theme_options') )
+		// add $cap capability to this role object
+		$role_object->add_cap( 'edit_theme_options' );
+}
+
+add_action( 'admin_init', 'cltvo_role_edit' );
