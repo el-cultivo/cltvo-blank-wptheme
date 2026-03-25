a679552894c7f68f93a447bde9d2b39c53a2532b
Len Serrano <len.serrano.g@gmail.com>
2026-03-25 11:38:52 -0600

    Vulnebarilidad de phpmailer, se actualiza versión de ese paquete

--- js/functions.js
+++ js/functions.js
@@ -6,21 +6,21 @@
 /******/ 	function __webpack_require__(moduleId) {
 /******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-/******/
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
 /******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+/******/ 		module.l = true;
 /******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
@@ -33,41 +33,118 @@
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
 /******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "./js/";
+/******/ 	__webpack_require__.p = "/";
+/******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/******/ ({
 
-	'use strict';
-	
-	var _constants = __webpack_require__(1);
-	
-	_constants.w.load(function () {
-	    console.log('Hello world from El Cultivo! --Load');
-	});
-	
-	(0, _constants.$)(document).ready(function () {
-	    console.log('Hello world from El Cultivo! --Ready');
-	});
+/***/ "./es6/constants.js":
+/*!**************************!*\
+  !*** ./es6/constants.js ***!
+  \**************************/
+/*! exports provided: $, w */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "$", function() { return $; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "w", function() { return w; });
+var $ = jQuery;
+var w = $(window);
 
 /***/ }),
-/* 1 */
+
+/***/ "./es6/micorriza.js":
+/*!**************************!*\
+  !*** ./es6/micorriza.js ***!
+  \**************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _constants__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./constants */ "./es6/constants.js");
+
+_constants__WEBPACK_IMPORTED_MODULE_0__["w"].load(function () {
+  console.log('Hello world from El Cultivo! --Load');
+});
+Object(_constants__WEBPACK_IMPORTED_MODULE_0__["$"])(document).ready(function () {
+  console.log('Hello world from El Cultivo! --Ready');
+});
+
+/***/ }),
+
+/***/ "./sass/mazorca.scss":
+/*!***************************!*\
+  !*** ./sass/mazorca.scss ***!
+  \***************************/
+/*! no static exports found */
 /***/ (function(module, exports) {
 
-	"use strict";
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	var $ = exports.$ = jQuery;
-	var w = exports.w = $(window);
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!****************************************************!*\
+  !*** multi ./es6/micorriza.js ./sass/mazorca.scss ***!
+  \****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! /Applications/MAMP/htdocs/el-cultivo/wp-content/themes/cltvo-blank-wptheme/es6/micorriza.js */"./es6/micorriza.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/el-cultivo/wp-content/themes/cltvo-blank-wptheme/sass/mazorca.scss */"./sass/mazorca.scss");
+
 
 /***/ })
-/******/ ]);
+
+/******/ });
 //# sourceMappingURL=functions.js.map
